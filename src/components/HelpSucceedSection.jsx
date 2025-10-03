b74605a7406dd70cc20566fdd363db1289805523
Hasnain <73098513+Shaikhasnain@users.noreply.github.com>
2025-10-03 12:26:27 +0530

    added success section svg files and change success section layout

--- src/components/HelpSucceedSection.jsx
+++ src/components/HelpSucceedSection.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import OnePlatform from "../assets/images/one-platform.png";
-import OnlineStore from "../assets/images/online-store.png";
-import TransparentSystem from "../assets/images/transparent-system.png";
-import ForEveryone from "../assets/images/for-everyone.png";
-import EasyGrowth from "../assets/images/easyy-growth.png";
+import allInOne from "../assets/images/success-icons/all-in-One.svg";
+import freeWebsite from "../assets/images/success-icons/free-website.svg";
+import realTime from "../assets/images/success-icons/real-time-tracking.svg";
+import integration from "../assets/images/success-icons/seamless-integration.svg";
+import shipRate from "../assets/images/success-icons/shipping-rates.svg";
 
 const HelpSucceedSection = () => {
   useEffect(() => {
@@ -53,65 +53,65 @@ const HelpSucceedSection = () => {
           <div className="container">
             <div className="row">
               <div className="col-12 col-lg-12">
-                <div className="grid__boxes">
-                  <div className="box-1 card">
+                <div className="grid__boxes help-success-cards">
+                  <div className="box-1 help-success-card">
+                    <div className="card__asset">
+                      <img src={allInOne} alt="one platform" />
+                    </div>
                     <div className="card__title text-center">
-                      <h4 className="h4">All-in-One Shipping Platform </h4>
+                      <h6 className="h6">All-in-One Shipping Platform </h6>
                       <p>
                         Access multiple courier partners on a single platform
                         for easy and fast shipping .
                       </p>
                     </div>
-                    <div className="card__asset">
-                      <img src={OnePlatform} alt="one platform" />
-                    </div>
                   </div>
-                  <div className="box-2 card">
+                  <div className="box-2 help-success-card">
+                    <div className="card__asset">
+                      <img src={shipRate} alt="Free Online Store" />
+                    </div>
                     <div className="card__title text-center">
-                      <h4 className="h4">Best Shipping Rates</h4>
+                      <h6 className="h6">Best Shipping Rates</h6>
                       <p>
                         Save money with discounted rates across India’s leading
                         courier services, ideal for wholesalers and retailers
                       </p>
                     </div>
-                    <div className="card__asset">
-                      <img src={OnlineStore} alt="Free Online Store" />
-                    </div>
                   </div>
-                  <div className="box-3 card">
+                  <div className="box-3 help-success-card">
+                    <div className="card__asset">
+                      <img src={freeWebsite} alt="Transparent System" />
+                    </div>
                     <div className="card__title text-center">
-                      <h4 className="h4">Free Website</h4>
+                      <h6 className="h6">Free Website</h6>
                       <p>
                         Start selling online instantly with a free website
                         connected to our shipping services.
                       </p>
                     </div>
-                    <div className="card__asset">
-                      <img src={TransparentSystem} alt="Transparent System" />
-                    </div>
                   </div>
-                  <div className="box-4 card">
+                  <div className="box-4 help-success-card">
+                    <div className="card__asset">
+                      <img src={integration} alt="For Everyone" />
+                    </div>
                     <div className="card__title text-center">
-                      <h4 className="h4">Seamless Integration</h4>
+                      <h6 className="h6">Seamless Integration</h6>
                       <p>
                         Connect your Shopify store to our platform for automatic
                         order processing, easy shipping management, and
                         real-time tracking
                       </p>
                     </div>
-                    <div className="card__asset">
-                      <img src={ForEveryone} alt="For Everyone" />
-                    </div>
                   </div>
-                  <div className="box-5 card">
+                  <div className="box-5 help-success-card">
+                    <div className="card__asset">
+                      <img src={realTime} alt="Easy Growth" />
+                    </div>
                     <div className="card__title text-center">
-                      <h4 className="h4">Real-Time Tracking</h4>
+                      <h6 className="h6">Real-Time Tracking</h6>
                       <p>
                         Keep your customers informed with live shipment updates.
                       </p>
-                    </div>
-                    <div className="card__asset">
-                      <img src={EasyGrowth} alt="Easy Growth" />
                     </div>
                   </div>
                 </div>
